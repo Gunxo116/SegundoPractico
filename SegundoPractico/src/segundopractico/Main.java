144858f158458abd803111d443606d62dda21c30
Esteban <esteban1.redon2@gmail.com>
2025-08-20 20:21:07 -0300

    Modificacion

--- SegundoPractico/src/segundopractico/Main.java
+++ SegundoPractico/src/segundopractico/Main.java
@@ -6,16 +6,7 @@ class EstebanRedonColab {
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 
-        System.out.println("Prueba de Git");
-
-        Test test = new Test("Juan", 25, "Perez");
-        System.out.println("Nombre: " + test.getNombre());
->>>>>>> fdf00da47b113ff0d01e6be513c53e57536ce3b4
-
-        
     }
 
 }
