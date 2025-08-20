c849c22f7510a03ae4ae76c8565202d727f0b405
Gunxo116 <yamilbarbeito10@gmail.com>
2025-08-20 19:53:35 -0300

    PruebaCLases

--- SegundoPractico/src/segundopractico/Test.java
+++ SegundoPractico/src/segundopractico/Test.java
@@ -4,4 +4,32 @@ public class Test {
     private int Edad;
     private String Apellido;
 
+    
+    public Test(String nombre, int edad, String apellido) {
+        Nombre = nombre;
+        Edad = edad;
+        Apellido = apellido;
+    }
+    
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public int getEdad() {
+        return Edad;
+    }
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+    public String getApellido() {
+        return Apellido;
+    }
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    
+
 }
