3e2e8d195ebb74df365f0b87ef4db43d6a79a2f6
Gunxo116 <yamilbarbeito10@gmail.com>
2025-08-20 20:15:51 -0300

    Update Main.java

--- SegundoPractico/src/segundopractico/Main.java
+++ SegundoPractico/src/segundopractico/Main.java
@@ -1,12 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Prueba de Git");
-        
-        Test test = new Test("Juan", 25, "Perez");
-        System.out.println("Nombre: " + test.getNombre());  
 
+        
     }
     
 }
