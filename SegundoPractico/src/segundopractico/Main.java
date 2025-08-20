64abbc01ae11552d2036d4b119f394f285fb7ace
Gunxo116 <yamilbarbeito10@gmail.com>
2025-08-20 20:22:13 -0300

    añadi clase yamil

--- SegundoPractico/src/segundopractico/Main.java
+++ SegundoPractico/src/segundopractico/Main.java
@@ -2,19 +2,18 @@ class EstebanRedonColab {
     public static void saludo() {
         System.out.println("Clase de prueba de colaboración con git");
     }
+
+class YamilBarbeitoColab {
+    public static void saludo() {
+        System.out.println("Clase de prueba de colaboración con git");
+    }
+}
+
+
 }
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-
-        System.out.println("Prueba de Git");
-
-        Test test = new Test("Juan", 25, "Perez");
-        System.out.println("Nombre: " + test.getNombre());
->>>>>>> fdf00da47b113ff0d01e6be513c53e57536ce3b4
-
         
     }
 
